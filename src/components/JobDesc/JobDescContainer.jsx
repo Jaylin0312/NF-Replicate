199ae2d6bff615df0c6b417147fb6ec800505df6
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-02-26 02:10:43 +0800

    feat: layout and card components with placeholder

---
+++ src/components/JobDesc/JobDescContainer.jsx
@@ -0,0 +1,10 @@
+function Desc() {
+  return (
+    <div className="pr-nf5 pb-[50px] overflow-x-hidden overflow-y-scroll max-h-info-card">
+      <div className="py-[29px] px-[20px] shadow-card rounded-lg bg-white">
+        Placeholder
+      </div>
+    </div>
+  );
+}
+export default Desc;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,31 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px',
+        'nf-sm': '440px',
+      },
+      colors: {
+        'nf-green1': '#DDF7E9',
+        'nf-green2': '#1fc76a',
+        'nf-grey1': '#838383',
+        'nf-grey2': '#f2f2f2',
+      },
+      boxShadow: {
+        'card': '0px 0px 2px rgb(0 0 0 / 25%)',
+        'card-hover': '0px 0px 2px rgb(0 0 0 / 25%), 0 3px 12px rgb(0 0 0 / 10%);'
+      },
+      padding:{
+        'nf5': '5px',
+      },
+      margin:{
+        'nf5': '5px',
+      },
+      maxHeight:{
+        'info-card': 'calc(100vh - 106px)',
+      },
+    },
   },
   plugins: [],
 }
