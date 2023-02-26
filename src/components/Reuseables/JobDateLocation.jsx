c2f713acec2b07a96f17f1e23ec6de5991488525
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-02-26 19:44:51 +0800

    fix: adjust UI according to real website

--- src/components/Reuseables/JobDateLocation.jsx
+++ src/components/Reuseables/JobDateLocation.jsx
@@ -2,7 +2,7 @@ function JobDateLocation(props) {
   return (
     <div >
       <div className="text-nf-grey1 flex">
-        <span className="text-nf-green2 font-extrabold mr-3">{props.created}</span>
+        <span className="text-nf-green2 font-bold mr-3">{props.created}</span>
         <div className="flex items-center">
           <img
             className="inline-block h-4 w-2.5 mr-1"
