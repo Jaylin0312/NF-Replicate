b6b75df18ee294b1c05ae444cd969cdea13398ea
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-02-27 03:34:45 +0800

    fix: ui fix for max-width

--- src/components/JobDesc/Container.jsx
+++ src/components/JobDesc/Container.jsx
@@ -5,7 +5,7 @@ import DescContent from "./Content";
 function JobDescContainer(props) {
   return (
     <div className="pr-nf5 pb-[50px] overflow-x-hidden overflow-y-scroll">
-      <div className="py-[29px] px-[20px] shadow-card rounded-lg bg-white lg:max-w-[660px]">
+      <div className="py-[29px] px-[20px] shadow-card rounded-lg bg-white">
         <DescHeader selectedJob={props.selectedJob} />
         <div className="w-breakline h-[1px] bg-nf-grey2 my-5 mx-[-20px]"></div>
         <DescMiddle selectedJob={props.selectedJob}/>
